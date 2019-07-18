aa662c5e90c85100795e85be5aaeab36281f15a5
Chinmaya Nathany <chinmaya.nahany@gmail.com>
2019-07-18 18:41:09 +0530

    Updated card

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ server.post('/get-movie-details', (req, res) => {
                             "buttons": [
                                 {
                                 "text": "Click Here for Movie information",
-                                "postback": encodeURI(`http://www.google.com/?q=${movieToSearch}&apikey=${API_KEY}`),
+                                "postback": encodeURI(`http://www.google.com/search?q=${movieToSearch}`),
                                 }
                             ]
 
