5112c24f71e17458016c00b2af48237989fa7054
Chinmaya Nathany <chinmaya.nahany@gmail.com>
2019-07-18 17:01:08 +0530

    Updated to API v2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -14,7 +12,7 @@ server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
 
-    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
+    const movieToSearch = req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
@@ -26,21 +24,59 @@ server.post('/get-movie-details', (req, res) => {
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
+            res.status(200);
+
             return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
-                source: 'get-movie-details'
+                fulfillmentText: dataToSend,
+                fulfillmentMessages:[
+                    {
+                        "card": {
+                            "title": movie.title,
+                            "subtitle": movie.Country,
+                            "imageUri": movie.Poster,
+                            "buttons": [
+                                {
+                                "text": "Buscar informacion",
+                                "postback": 'https://www.google.com/search?q$={encodeURI(movieToSearch)}'
+                                }
+                            ]
+
+                        }
+                    }
+
+                ],
+payload: {
+    "google": {
+        "expectUserResponse": true,
+        "richResponse": {
+            "items": [
+                {
+                    "simpleResponse": {
+                        "textToSpeech": dataToSend
+                    }
+                }
+            ]
+        }
+    },
+    "facebook": {
+        "text": dataToSend
+    },
+    "slack": {
+        "text": dataToSend
+    }
+},
+source: 'get-movie-details'
             });
         });
     }, (error) => {
+        res.status(200);
         return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
+            fulfillmentText: 'Something went wrong!',
             source: 'get-movie-details'
         });
     });
 });
 
-server.listen((process.env.PORT || 8000), () => {
-    console.log("Server is up and running...");
+server.listen((process.env.PORT || 8800), () => {
+    console.log('Server is up and running in port${process.env.PORT || 8800}');
 });
